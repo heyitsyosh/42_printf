c7a0eadace445d17802dca6d74ce184e2692d120
Maria Yoshikawa <myoshika@c1r24s3.42tokyo.jp>
2022-08-12 04:47:19 +0900

    update

--- printf/srcs/flags.c
+++ printf/srcs/flags.c
@@ -6,60 +6,47 @@
 /*   By: myoshika <myoshika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 04:15:35 by myoshika          #+#    #+#             */
-/*   Updated: 2022/08/11 04:59:22 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/08/12 02:44:55 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 
-static int	is_whitespace(char c)
+static long	make_l(const char *str, size_t *i, long num)
 {
-	return (c == 32 || (9 <= c && c <= 13));
-}
-
-static long	make_l(const char *str, size_t i, int sign, long num)
-{
-	//(SIZE_T_MAX) 
-	while (*(str + i) && (*(str + i) >= '0' && *(str + i) <= '9'))
+	while (*(str + *i) && (*(str + *i) >= '0' && *(str + *i) <= '9'))
 	{
-		if (sign == 1 && ((num > LONG_MAX / 10)
-				|| (num == LONG_MAX / 10 && *(str + i) - '0' > LONG_MAX % 10)))
+		if ((num > LONG_MAX / 10)
+			|| (num == LONG_MAX / 10 && *(str + *i) - '0' > LONG_MAX % 10))
 			return (LONG_MAX);
-		else if (sign == -1 && ((num < min / 10)
-				|| (num == min / 10 && *(str + i) - '0' > min % 10 * -1)))
-			return (LONG_MIN);
-		num = (num * 10) + sign * (*(str + i) - '0');
+		num = (num * 10) + (*(str + *i) - '0');
 		i++;
 	}
 	return (num);
 }
 
-int	printf_atoi(const char *str)
+size_t	printf_atoi(const char *str, t_printinfo *info)
 {
 	size_t	i;
-	int		sign;
 	long	num;
 
-	sign = 1;
 	i = 0;
-	while (is_whitespace(*(str + i)))
-		i++;
-	if ((*(str + i) == '+' || *(str + i) == '-') && *(str + i))
-	{
-		if (*(str + i) == '-')
-			sign = -1;
-		i++;
-	}
-	num = make_l(str, i, sign, 0);
-	return ((int)num);
+	num = make_l(str + 1, &i, 0);
+	if (*str == '*')
+		info->width = num;
+	if (*str == '.')
+		info->precision = num;
+	if (*str == '0')
+		info->zero = num;
+	else if (ft_isdigit(*str))
+		info->width = num;
+	return (i);
 }
+//what if after the flag is \0
 
-size_t	check_flags(char *after_pct, t_printinfo *info)
+size_t	check_flags(char *after_pct, t_printinfo *info, size_t i)
 {
-	size_t		i;
-
-	i = 0;
 	while (!ft_strchr("cspdiuxX%", *(after_pct + i)))
 	{
 		if (*(after_pct + i) == '-')
@@ -73,13 +60,14 @@ size_t	check_flags(char *after_pct, t_printinfo *info)
 		if (!ft_strchr("*.0123456789", *(after_pct + i)))
 			i++;
 		if (*(after_pct + i) == '*')
-			i += find_flag_num(*(after_pct + i), info);
+			i += printf_atoi(*(after_pct + i), info);
 		else if (*(after_pct + i) == '.')
-			i += find_flag_num(*(after_pct + i), info);
+			i += printf_atoi(*(after_pct + i), info);
 		else if (*(after_pct + i) == '0')
-			i += find_flag_num(*(after_pct + i), info);
+			i += printf_atoi(*(after_pct + i), info);
 		else if (ft_isdigit(*(after_pct + i)))
-			i += find_flag_num(*(after_pct + i), info);
+			i += printf_atoi(*(after_pct + i), info);
 	}
+	info->i += i;
 	return (i);
 }
--- printf/srcs/no_specifier.c
+++ printf/srcs/no_specifier.c
@@ -6,35 +6,34 @@
 /*   By: myoshika <myoshika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 17:21:29 by myoshika          #+#    #+#             */
-/*   Updated: 2022/08/11 01:19:07 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/08/12 02:41:04 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-void	print_str(char *s, int fd, size_t s_len)
-{
-	while (s_len > INT_MAX)
-	{
-		write(fd, s, INT_MAX);
-		s += INT_MAX;
-		s_len -= INT_MAX;
-	}
-	write(fd, s, s_len);
-}
-
-size_t	no_conversion(char *input, t_printinfo *info)
+long	no_conversion(char *input, t_printinfo *info)
 {
 	char	*percent_ptr;
+	size_t	print_len;
 	size_t	len;
 
 	percent_ptr = ft_strchr(input, '%');
-	if (!percent_ptr)
-		len = ft_strlen(input);
+	len = ft_strlen(input);
+	if (input + INT_MAX <= percent_ptr || (!percent_ptr && INT_MAX <= len))
+	{
+		info->error = true;
+		print_len = 0;
+		info->i += percent_ptr - input;
+	}
+	else if (!percent_ptr)
+		print_len = len;
 	else
-		len = percent_ptr - input;
-	print_str(input, 1, len);
-	info->i += len;
-	return (len);
+		print_len = percent_ptr - input;
+	write(1, input, print_len);
+	info->i += print_len;
+	return (print_len);
 }
+
+//check literal strings bigger than intmax
