2fe8fc3464dc6d2434dace2285cf430a343f8219
heyitsyosh <47554536+heyitsyosh@users.noreply.github.com>
2022-08-13 04:53:39 +0900

    Update ft_printf.c

--- printf/srcs/ft_printf.c
+++ printf/srcs/ft_printf.c
@@ -6,18 +6,35 @@
 /*   By: myoshika <myoshika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 05:44:37 by myoshika          #+#    #+#             */
-/*   Updated: 2022/08/12 02:42:41 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/08/13 02:59:30 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-size_t	conversion(char *specifiers, t_printinfo *info, va_list args)
+size_t	conversion(char *specifiers, t_printinfo *info)
 {
 	int	printed_char_count;
 
-	specifiers = check_flags(specifiers, info, 0);
-
+	info->current_specifier = check_flags(specifiers, info, 0);
+	if (!info->current_specifier)
+	{
+		info->error = true;
+		info->i++;
+		return (0);
+	}
+	switch(info->current_specifier)
+	{
+		case 'c':
+		case 's':
+		case 'p':
+		case 'd':
+		case 'i':
+		case 'u':
+		case 'x':
+		case 'X':
+		case '%':
+	}
 	return (printed_char_count);
 }
 
@@ -37,21 +54,20 @@ int	ft_printf(const char *input, ...)
 {
 	size_t		tmp;
 	t_printinfo	*info;
-	va_list		args;
 
 	info->printed = 0;
 	info->i = 0;
-	va_start(args, input);
+	va_start(info->args, input);
 	while (*(input + info->i))
 	{
 		init(info);
 		if (*(input + info->i) == '%')
-			tmp += conversion(input + info->i++, info, args); //test the ++ bit
+			tmp += conversion(input + info->i++, info); //test the ++ bit
 		else
 			tmp += no_conversion(input + info->i, info);
 		info->printed += tmp;
 	}
-	va_end(args);
+	va_end(info->args);
 	if (info->printed >= INT_MAX || info->error == true)
 		return (-1);
 	return (info->printed);
@@ -60,3 +76,5 @@ int	ft_printf(const char *input, ...)
 //check overflow before addition, dont know how real printf handles int overflows yet
 //if i send (input + i + 1) and recieve it as a pointer, if I move the pointer in that function, input changes right?
 //idk confirm that later
+
+//how to use args??
