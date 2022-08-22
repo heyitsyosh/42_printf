c6062af4073fcf75c213a83562a49ba50ae69bb6
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-08-22 11:53:13 +0900

    update

--- printf/flags.c
+++ printf/flags.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 07:01:55 by myoshika          #+#    #+#             */
-/*   Updated: 2022/08/22 10:13:21 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/08/22 11:39:00 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,5 @@ size_t	get_info(const char *after_pct, t_info *info, va_list args)
 		return (INT_MAX);
 	else
 		info->fmt = after_pct[i++];
-	if (info->dash && info->padding == '0')
-		info->padding = ' ';
 	return (i);
 }
