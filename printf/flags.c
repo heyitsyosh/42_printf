a88897a2f9e6751b5f0700056d84672f5ff4dc23
maria <winterzaq1@gmail.com>
2022-08-22 09:10:53 +0900

    guacamole's printf ignores precision if over INT_MAX, although local returns -1

--- printf/flags.c
+++ printf/flags.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 07:01:55 by myoshika          #+#    #+#             */
-/*   Updated: 2022/08/21 17:20:34 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/08/22 09:04:03 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,5 +100,7 @@ size_t	get_info(const char *after_pct, t_info *info, va_list args)
 		info->fmt = after_pct[i++];
 	if (info->dash && info->padding == '0')
 		info->padding = ' ';
+	if (info->precision == INT_MAX)
+		info->precision = -1;
 	return (i);
 }
